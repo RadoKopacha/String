46e57f8152d2054f933cecc6ae5085d812be60c1
RadoKopacha <petyrnikolov0804@gmail.com>
2024-03-19 20:27:25 +0200

    String

---
+++ VerticalWrpds/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace VerticalWrpds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string words = Console.ReadLine();
+            char[] result = words.ToCharArray();
+            Console.WriteLine(string.Join("\n",result));
+        }
+    }
+}
